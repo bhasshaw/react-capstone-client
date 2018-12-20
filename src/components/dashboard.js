725a138256ba486f0a7abd994d0107776bccab96
Bryce Hasshaw <bhasshaw@gmail.com>
2018-12-19 21:54:25 -0800

    styling

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -8,28 +8,30 @@ import check from '../views/check.png';
 export class Dashboard extends React.Component {
     render() {
         return (
-        <div className="position-relative p-4 my-2 text-center bg-dark">
-            <h3 className="mb-3">Hello, {this.props.name}!</h3>
+        <div className="position-relative px-4 my-2 text-center bg-dark">
+        <div className="my-5">
+            <h2 className="mb-5">Hello, {this.props.name}!</h2>
             <div className="row">
                 <div className="col-lg-4">
-                    <img className="rounded-circle" src={calendar} alt="Generic placeholder" width="140" height="140"></img>
-                    <h2 className="pt-3">Schedule</h2>
+                    <img className="rounded-circle" src={calendar} alt="Generic placeholder" width="160" height="160"></img>
+                    <h2 className="my-4">Schedule</h2>
                     <p>With DogDates you can schedule your own play date here in the Portland area! Easily enter the park name, date, and time that  
                         you will be playing with your dog and let other dogs join while you're there!</p>
                 </div>
                 <div className="col-lg-4">
-                    <img className="rounded-circle" src={magnify} alt="Generic placeholder" width="140" height="140"></img>
-                    <h2 className="pt-3">Find</h2>
+                    <img className="rounded-circle" src={magnify} alt="Generic placeholder" width="160" height="160"></img>
+                    <h2 className="my-4">Find</h2>
                     <p>You can also search for play dates in Portland! Find play dates that other dog owners have 
                         scheduled and meet up with them!</p>
                 </div>
                 <div className="col-lg-4">
-                    <img className="rounded-circle" src={check} alt="Generic placeholder" width="140" height="140"></img>
-                    <h2 className="pt-3">Meet Dogs</h2>
+                    <img className="rounded-circle" src={check} alt="Generic placeholder" width="160" height="160"></img>
+                    <h2 className="my-4">Meet Dogs</h2>
                     <p>Whether you schedule your own play date or join others, your dog will have a great time 
                         meeting new friends!</p>
                 </div>
             </div>
+        </div>
         </div>
         );
     }
