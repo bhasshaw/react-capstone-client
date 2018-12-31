cea378e6268215514333462483a4a741c830a43b
Bryce Hasshaw <bhasshaw@gmail.com>
2018-12-30 18:32:10 -0800

    changed verbage

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -15,13 +15,13 @@ export class Dashboard extends React.Component {
                 <div className="col-lg-4">
                     <img className="rounded-circle" src={calendar} alt="Generic placeholder" width="160" height="160"></img>
                     <h2 className="my-4">Schedule</h2>
-                    <p>With DogDates you can schedule your own play date here in the Portland area! Easily enter the park name, date, and time that  
-                        you will be playing with your dog and let other dogs join while you're there!</p>
+                    <p>With DogDates you can schedule your own play date! Easily enter the park name, date, and time you will be playing
+                         with your dog and let others join!</p>
                 </div>
                 <div className="col-lg-4">
                     <img className="rounded-circle" src={magnify} alt="Generic placeholder" width="160" height="160"></img>
                     <h2 className="my-4">Find</h2>
-                    <p>You can also search for play dates in Portland! Find play dates that other dog owners have 
+                    <p>You can also search for play dates! Find play dates that other dog owners have 
                         scheduled and meet up with them!</p>
                 </div>
                 <div className="col-lg-4">
