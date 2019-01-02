4c18155734d01aca23eea137ae06521651c2ce1d
Bryce Hasshaw <bhasshaw@gmail.com>
2019-01-01 20:19:10 -0800

    change to google maps api

--- src/components/google.js
+++ src/components/google.js
@@ -4,17 +4,7 @@ const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 export class MapContainer extends React.Component {
     render() {
       return (
-        <Map google={this.props.google} zoom={14}>
-   
-          {/* <Marker onClick={this.onMarkerClick}
-                  name={'Current location'} />
-   
-          <InfoWindow onClose={this.onInfoWindowClose}>
-              <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-              </div>
-          </InfoWindow> */}
-        </Map>
+        <Map google={this.props.google} zoom={14}></Map>
       );
     }
   }
