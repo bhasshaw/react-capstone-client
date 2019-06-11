66914c17eccb8f75cbbddd668b6d5f8613cf6083
Bryce Hasshaw <bhasshaw@gmail.com>
2019-06-10 21:33:27 -0700

    removed login title

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -5,17 +5,14 @@ import {Redirect} from 'react-router-dom';
 import LoginForm from './login-form';
 
 export function LandingPage(props) {
-    
+
     if (props.loggedIn) {
         return <Redirect to="/dashboard" />;
     }
 
     return (
         <div className="text-center bg-dark my-2 login">
-            <div className="mx-5 mt-5">
-                <h4>Please log in</h4>
-            </div>
-            <LoginForm /> 
+            <LoginForm />
         </div>
     );
 }
